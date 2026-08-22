c9313a42c739aa7e19d0cb0dda6cf4972d4e1fa8
Votline <quogg2@gmail.com>
2026-08-22 14:45:22 +0300

    feat(gateway): implement circuit breaker package
    - Create 'cbreaker' package with 'sony/gobreaker' implementation
    - Add cbreaker envs to '.env_vars' and license

---
+++ gateway/internal/cbreaker/cb.go
@@ -0,0 +1,55 @@
+// Package cbreaker cb.go contains methods for
+// create circuit breaker
+package cbreaker
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/sony/gobreaker/v2"
+	"go.uber.org/zap"
+)
+
+// getEnvInt returns the value of the environment variable
+// or the default value if the environment variable is not set
+func getEnvInt(key string, defaultVal int) int {
+	valStr := os.Getenv(key)
+	if valStr == "" {
+		return defaultVal
+	}
+	val, err := strconv.Atoi(valStr)
+	if err != nil {
+		return defaultVal
+	}
+	return val
+}
+
+// NewCB creates a new circuit breaker
+// with values from environment variables
+func NewCB(name string, log *zap.Logger) *gobreaker.CircuitBreaker[any] {
+	const op = "cbreaker.NewCB"
+
+	maxRequests := uint32(getEnvInt("CB_MAX_REQUESTS", 5))
+	timeout := time.Duration(getEnvInt("CB_TIMEOUT", 20))
+	interval := time.Duration(getEnvInt("CB_INTERVAL", 30))
+	failCnt := uint32(getEnvInt("CB_FAIL_CNT", 5))
+
+	st := gobreaker.Settings{
+		Name:        name,
+		MaxRequests: maxRequests,
+		Timeout:     timeout * time.Second,
+		Interval:    interval * time.Second,
+		ReadyToTrip: func(counts gobreaker.Counts) bool {
+			return counts.ConsecutiveFailures >= failCnt
+		},
+		OnStateChange: func(name string, from gobreaker.State, to gobreaker.State) {
+			log.Info("Circuit breaker state changed",
+				zap.String("name", name),
+				zap.String("from", from.String()),
+				zap.String("to", to.String()),
+				zap.String("op", op))
+		},
+	}
+	return gobreaker.NewCircuitBreaker[any](st)
+}
